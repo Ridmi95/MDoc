4049636b2f2e33ee5791aab220ad22e01cc76b18
Nishiki97 <nsihikiyapa1997@gmail.com>
2019-08-03 17:43:56 +0530

    nishiki commit javaClasses

---
+++ app/src/main/java/com/example/mdoc/MyPatientsActivity.java
@@ -0,0 +1,16 @@
+package com.example.mdoc;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MyPatientsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_patients);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
