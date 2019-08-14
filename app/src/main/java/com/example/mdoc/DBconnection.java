c020b4e5b6843e2a40d905de8cdfa8ccff4da03b
Ridmi95 <yrridmi1995@gmail.com>
2019-08-14 09:36:05 +0530

    DB connection Class

---
+++ app/src/main/java/com/example/mdoc/DBconnection.java
@@ -0,0 +1,21 @@
+package com.example.mdoc;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBconnection extends SQLiteOpenHelper {
+    public DBconnection(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
