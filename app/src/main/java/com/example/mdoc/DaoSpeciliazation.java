f3e5f74fc57360c4683f9d42de48344561520b8c
DJ <dinushe22@gmail.com>
2019-09-06 15:15:23 +0530

    dinushe

---
+++ app/src/main/java/com/example/mdoc/DaoSpeciliazation.java
@@ -0,0 +1,68 @@
+package com.example.mdoc;
+
+public class DaoSpeciliazation {
+    //creating variables for specialization
+    private int specializationId;
+    private String specializationName;
+    private String specializationDepartment;
+    private String specializationDescription;
+
+    public DaoSpeciliazation() {
+    }
+
+    //getters and setters
+
+    public int getSpecializationId() {
+        return specializationId;
+    }
+
+    public void setSpecializationId(int specializationId) {
+        this.specializationId = specializationId;
+    }
+
+    public String getSpecializationName() {
+        return specializationName;
+    }
+
+    public void setSpecializationName(String specializationName) {
+        this.specializationName = specializationName;
+    }
+
+    public String getSpecializationDepartment() {
+        return specializationDepartment;
+    }
+
+    public void setSpecializationDepartment(String specializationDepartment) {
+        this.specializationDepartment = specializationDepartment;
+    }
+
+    public String getSpecializationDescription() {
+        return specializationDescription;
+    }
+
+    public void setSpecializationDescription(String specializationDescription) {
+        this.specializationDescription = specializationDescription;
+    }
+
+
+    //method implementation need to be done
+
+    public boolean addSpecialzation(){
+
+        return true;
+    }
+
+    public boolean updateSpecialzation()
+    {
+        return true;
+    }
+
+    public boolean deleteSpecialization()
+    {
+        return true;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/mdoc/InsertFragment.java
@@ -0,0 +1,32 @@
+package com.example.mdoc;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@linkInsertFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@linkInsertFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InsertFragment extends Fragment {
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_insert, container, false);
+    }
+
+
+}
